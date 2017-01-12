d53b06ece33f10e9dd4a00077db858110c61f544
Chirath02 <chirath.02@gmail.com>
2017-01-12 09:44:50 +0530

    demo program 3

--- demoPrograms/2-add-two-numbers.asm
+++ demoPrograms/2-add-two-numbers.asm
@@ -10,16 +10,16 @@
 
 BITS 64
 
-extern printf
-extern scanf
+extern printf                           ; external functions
+extern scanf                       
 
 section .rodata
-    inp   :    db "%d"                  ; input format for scanf
+    inp   :    db "%d", 0               ; input format for scanf
     out   :    db "Sum is %d", 10, 0    ; output format for printf
     prompt:    db "Enter number: ", 0   ; prompt to enter input
 
-section .bss
-    num1: resq 1                        ; number 1
+section .bss                            ; uninitialised variables
+    num1: resq 1                        ; number 1            ; resq - reserve q word, 8 byte;(pointer to 8 bytes)
     num2: resq 1                        ; number 2
 
 section .text
@@ -32,22 +32,22 @@ section .text
         mov rdi, prompt             ; display prompt
         call printf
 
-        mov rsi, num1               ; 2nd arg of scanf: address of num1
-        mov rdi, inp                ; 1st arg of scanf: input format
-        call scanf                  ; call scanf
+        mov rsi, num1               ; 2nd arg of scanf: address of num1 - pointer to the variable
+        mov rdi, inp                ; 1st arg of scanf: input format  - "%d"
+        call scanf                  ; call scanf, scanf("%d", a), a is a pointer
 
         mov rdi, prompt             ; display prompt
         call printf
 
         mov rsi, num2               ; 2nd arg of scanf: address of num2
         mov rdi, inp                ; 1st arg of scanf: input format
-        call scanf                  ; call scanf
+        call scanf                  ; call scanf, read 2nd number
 
-        mov rsi, QWORD [num1]       ; Copy num1's value to rsi
+        mov rsi, QWORD [num1]       ; Copy num1's value to rsi - QWORD(8bytes) - read 8byte value from num1 pointer
         add rsi, QWORD [num2]       ; Add num2's value to rsi
-        mov rdi, out                ; 1st arg of printf: output format to display sum
+        mov rdi, out                ; 1st arg of printf: output format to display sum(out = "sum is %d"
         call printf                 ; call printf
 
-        xor rax, rax                ; same as "mov rax, 0"
+        xor rax, rax                ; same as "mov rax, 0" ,xor with same value = 0
         leave                       ; same as "mov rsp, rbp; pop rbp"
         ret
