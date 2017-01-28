4ff110b98589703365ba85f72124e0f332c4e688
Chirath02 <chirath.02@gmail.com>
2017-01-28 11:35:09 +0530

    finsihed part 1 of q1

--- hw1/get-env.asm
+++ hw1/get-env.asm
@@ -1,6 +1,6 @@
 ; Author    :   Chirath R                                                      ;
 ; Date      :   32/01/2017                                                     ;
-; Program   :   print "Hello world" using printf                               ;
+; Program   :   Environment variable query tool                               ;
 ; Note      :   This program was tested on debain 8.6 64 bit using             ;
 ;               nasm 2.11.05 and gcc 4.9.2.                                    ;
 ;                                                                              ;
@@ -8,30 +8,43 @@
 
 BITS 64
 
-extern printf
-extern getenv                  ; declare that printf is defined elsewhere
+extern printf           ; declare printf
+extern getenv           ; getenv function to get an env
 
-section .rodata                             ; start of data section
-    hello_world: db "Hello, world!", 10, 0  ; string we wish to print out, always will be a pointer
+section .rodata
+    var1 : db "%s Not found", 10, 0
 
-    section .text                               ; start of text section
-        global main                             ; declare main as globally visible
+section .text                               ; start of text section
+    global main                             ; declare main as globally visible
 
     main:
-	push rbp                            ; set up main's stack frame
-	mov rbp, rsp                        ; on top of it's caller's frame
+	      push rbp                            ; set up main's stack frame
+	      mov rbp, rsp                        ; on top of it's caller's frame
 
-	mov r11, [rdx]
+        cmp r10, 2
+        jne .argumentPassed
 
-	.label:
-		mov rdi, r11
-		call printf
-		add r11, 8
-		jmp .label
+        .argumentPassed:
+    	      mov r11, [rsi + 8]                  ; pass commandline argument to getenv
+            mov rdi, r11
+            call getenv                         ; call getenv
 
+            test rax, rax
+            jz .notFound
 
+            mov rdi, rax                        ; pass return value of getenv to printf
+            call printf
 
-	mov rax, 0                          ; set return value
-	mov rsp, rbp                        ; destroy main's stack frame and
-	pop rbp                             ; restore main's caller's stack frame
-	ret
+            jmp .end
+
+        .notFound:
+            mov rdi, var1
+            mov rsi, r11
+            call printf
+
+        .end:
+
+    	  mov rax, 0                          ; set return value
+    	  mov rsp, rbp                        ; destroy main's stack frame and
+        pop rbp                             ; restore main's caller's stack frame
+    	  ret
