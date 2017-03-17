7c4fc14c5fee31b32cff6ada334a1c26b3d49493
Chirath02 <chirath.02@gmail.com>
2017-03-17 16:59:32 +0530

    Addisgnment half donbe

--- Assignment_3_Software-reverse-engineering/u4cse14019/keygen.py
+++ Assignment_3_Software-reverse-engineering/u4cse14019/keygen.py
@@ -1,6 +1,82 @@
 import md5
+from struct import *
+
+def xor_strings(xs, ys):
+    return "".join(chr(ord(x) ^ ord(y)) for x, y in zip(xs, ys))
 
 s = 'chirath'
 key =  md5.new(s).digest()
 
-print repr(key)
+key = map(hex,unpack('<QQ',key))
+
+key_left = key[0][2:-1]
+key_right = key[1][2:]
+
+binary_a = key_left.decode("hex")
+binary_b = key_right.decode("hex")
+
+key = xor_strings(binary_a, binary_b).encode("hex")
+
+key = int(key, 16)
+
+rax = 0
+ecx = 0
+
+array = [0x5daac65f720be8c9,
+            0x8c92b50d1c2d1e4f,
+            0xa6fd433408510ea4,
+            0x0e9296eddd450b03,
+            0x84db12124b70fbd9,
+            0x091d7b508606ffac,
+            0xfd41e1b9fd423b8f,
+            0x56bd6aca483c8c74,
+            0x037c7f5864b34642,
+            0xb488202e8ea9111b,
+            0x88af43a6fe478cda,
+            0xaa4fc294b8b380b6,
+            0x5b1c0d7ed87b9535,
+            0x4a9b68be4888bc63,
+            0x0b1a4657555b0964,
+            0xf0e4afaab7a436c1,
+            0x3b031b0100000408
+        ]
+
+for i in [0, 16, 32, 48]:
+    tmp = key
+    tmp = tmp >> i
+    tmp = tmp & 0xf
+    rax = rax ^ array[tmp]
+
+rdx = 0xda57e1b4b758031a
+
+rax = rax * rdx
+
+rax = int(str(hex(rax))[18:-1], 16)
+
+rdx = 0xa508de475239764c
+
+rax = rax + rdx
+
+rax = int(str(hex(rax))[3:-1], 16)
+
+rsi = 0xda57e1b4b758031a
+
+rcx = rdx
+
+s = [0] * 18
+
+s[0] = rax
+
+# loop 2
+
+for i in range(7):
+    rdx = rsi
+    rdx = rdx * s[i]
+    a = str(hex(rdx))[-17:]
+    rdx = int(a[:-1], 16)
+    rdx = rdx + rcx
+    a = str(hex(rdx))[-17:]
+    rdx = int(a[:-1], 16)
+    s[i+1] = rdx
+
+# loop 3
